56af6ef340a20dbc48d9d6cceeacd128dc3b0fa1
Kristof <nagykris68@gmail.com>
2025-04-23 23:15:13 +0200

    tablazat hozzaadasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,14 +1,41 @@
-class Area{//area class
-    constructor(ClassName){
+class Area {//area class
+    #div //property letrehozása
+
+    get div() {//getter a propertyhoz
+        return this.#div;//visszaadja a propertyt
+    }
+
+
+    constructor(ClassName) {//osztály neve
         let divcontainer = document.querySelector('.containeroop');//lekérdezi a container osztályú elemet
-        if(!divcontainer){//ha nem létezik
+        if (!divcontainer) {//ha nem létezik
             divcontainer = document.createElement('div');//létrehoz egy divet
             divcontainer.className = 'containeroop';//beállítja az osztálynevet
             document.body.appendChild(divcontainer);//hozzáadja a bodyhoz
         }
-        const div = document.createElement('div');//létrehoz egy divet
-        div.className = ClassName;//beállítja az osztálynevet
-        divcontainer.appendChild(div);//hozzáadja a divcontainerhez
+        this.#div = document.createElement('div');//létrehoz egy divet
+        this.#div.className = ClassName;//beállítja az osztálynevet
+        divcontainer.appendChild(this.#div);//hozzáadja a divcontainerhez
     }
-
 }
+
+class Table extends Area {//table class
+    constructor(cssClass) {//osztály neve
+        super(cssClass);//hívja a szülő osztály konstruktorát
+        const table = document.createElement('table');//létrehoz egy táblázatot
+        this.div.appendChild(table);//hozzáadja a divhez
+        const tablehead = document.createElement('thead');//létrehoz egy thead elemet
+        table.appendChild(tablehead);//hozzáadja a táblázathoz
+        const fejlecsor = document.createElement('tr');//létrehoz egy tr elemet
+        tablehead.appendChild(fejlecsor);//hozzáadja a theadhez
+        const fejleccella = ['szerzo', 'mufaj', 'cimm'];//létrehoz egy tömböt a fejléc celláinak
+        for (const cellatartalom of fejleccella) {//végigmegy a cellatartalmon
+            const thcella = document.createElement('th');//létrehoz egy th elemet
+            thcella.innerText = cellatartalom;//beállítja a cella tartalmát
+            fejlecsor.appendChild(thcella);//hozzáadja a trhez
+        }
+        const tbody = document.createElement('tbody');//létrehoz egy tbody elemet
+        table.appendChild(tbody);//hozzáadja a táblázathoz
+    }
+}
+
--- oop/main.js
+++ oop/main.js
@@ -1,4 +1,4 @@
 const elvalaszto = document.createElement('hr');// létrehoz egy hr elemet   
 document.body.appendChild(elvalaszto);// hozzáadja a bodyhoz
-const table = new Area('table');// létrehoz egy Area objektumot a table osztállyal
+const table = new Table('table');// létrehoz egy Area objektumot a table osztállyal
 const form = new Area('form');// létrehoz egy Area objektumot a form osztállyal
